1876240bc49cbe26a35c92cdc51ba26b235280a2
Njengak993 <njengak993@gmail.com>
2025-08-03 21:52:50 +0300

    a few changjegs

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
   base: '/SeedSavers/', // <--- This is essential for GitHub Pages
+  plugins: [react()],
 })
