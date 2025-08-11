12b9a4dc82ef39ef6a31507d35838fb6b1cd493b
Njengak993 <njengak993@gmail.com>
2025-08-11 19:29:08 +0300

    a few changess

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/SeedSavers/', 
+  base: '/SeedSavers/', // must match GitHub repo name
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
+  server: {
+    port: 5173,
+    strictPort: true,
+  },
+});
