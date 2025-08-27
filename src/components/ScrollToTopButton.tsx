ff99da09a84b8da8efb311984c5d3557069ec6f4
Njengak993 <njengak993@gmail.com>
2025-08-27 10:03:11 +0300

    a few change

---
+++ src/components/ScrollToTopButton.tsx
@@ -0,0 +1,39 @@
+// ScrollToTopButton.tsx
+import { useEffect, useState } from "react";
+import { FaArrowUp } from "react-icons/fa";
+import '../styles/scroll.css';
+
+const ScrollToTopButton = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  // Show button when user scrolls down
+  useEffect(() => {
+    const toggleVisibility = () => {
+      if (window.scrollY > 300) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+    window.addEventListener("scroll", toggleVisibility);
+    return () => window.removeEventListener("scroll", toggleVisibility);
+  }, []);
+
+  // Smooth scroll back to top
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  return (
+    isVisible && (
+      <button className="scroll-to-top" onClick={scrollToTop}>
+        <FaArrowUp />
+      </button>
+    )
+  );
+};
+
+export default ScrollToTopButton;
