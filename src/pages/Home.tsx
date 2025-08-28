0ba7b803000978b1aa88c74bf1b8976784b47294
Njengak993 <njengak993@gmail.com>
2025-08-28 13:27:12 +0300

    a few change

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,14 +2,14 @@
 import Hero from '../components/Hero';
 import WhoWeAre from '../components/WhoWeAre';
 import '../styles/home.css';
-import EventPopup from '../components/EventPopup';
+
 
 
 const Home = () => {
   return (
     <div className="home-page">
       {/* Replaced the old hero section with the new Hero component */}
-      <EventPopup />
+    
         <Hero />
       {/* Who We Are Section */}
       <WhoWeAre />
