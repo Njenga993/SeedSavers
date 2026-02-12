676e9b741c0631e8d722fd39e99895192a21cb25
Njengak993 <njengak993@gmail.com>
2026-02-12 12:12:45 +0300

    added the payment gateway paypal using paypal

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import { PayPalScriptProvider } from "@paypal/react-paypal-js";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App /> {/* No <BrowserRouter> here */}
+    <PayPalScriptProvider
+ options={{
+    "clientId": "AXAfJ5eLBnG2XzGkPGUkTUzuleVl2pu2MSxEX8QMqraXwj-bP1ZfUd5g21YawBT3-e8yGGwQIx_5-BLH",
+    currency: "USD",
+  }}
+>
+  <App /> {/* No <BrowserRouter> here */}
+</PayPalScriptProvider> 
   </React.StrictMode>
 );
